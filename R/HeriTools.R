834a673627b1049f234c9051631f2707915758c6
wjshi <sleepyjelly@gmail.com>
2016-12-06 13:13:36 -0700

    Remove unwanted libaray

--- R/HeriTools.R
+++ R/HeriTools.R
@@ -20,11 +20,6 @@
 # source("https://bioconductor.org/biocLite.R")
 # biocLite("DESeq2")
 
-
-# requireNamespace("statmod", quietly = TRUE)
-# requireNamespace("Matrix", quietly = TRUE)
-# requireNamespace("minqa", quietly = TRUE)
-
 requireNamespace("lme4", quietly = TRUE)
 requireNamespace("glmmADMB", quietly = TRUE)
 requireNamespace("tweedie", quietly = TRUE)
